0796e0fdcc8ba9ff8d2143154e387429620f64ef
Игорь Кошкарев <igorkoshkarev4qqq@mail.ru>
2023-04-29 16:36:16 +0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   yens_algorithm.cpp

--- yens_algorithm.cpp
+++ yens_algorithm.cpp
@@ -2,20 +2,24 @@
 
 using namespace std;
 
+const unsigned long long INF64 = 6e18;
+
 // функция возвращает пару из {distance, path}, где 
 // distance -- длина минимального пути от 0 до n - 1,
 // path -- сам путь
 
 template<typename container_of_pil>
-pair<unsigned long long, vector<int>> default_dijksrta(const vector<container_of_pil> &g){
-    const unsigned long long INF64 = 6e18;
-
+pair<unsigned long long, vector<int>> default_dijksrta(
+    const vector<container_of_pil>& g,
+    int start,
+    const unordered_set<int>& deleted_vertex
+){
     auto n = g.size();
     vector<unsigned long long> distance(n, INF64);
     vector<int> parent(n, -1);
-    distance[0] = 0;
+    distance[start] = 0;
     priority_queue<pair<unsigned long long, int>, vector<pair<unsigned long long, int>>, greater<pair<unsigned long long, int>>> q;
-    q.push({0ull, 0});
+    q.push({0ull, start});
 
     while(q.size()){
         auto temp = q.top();
@@ -28,7 +32,7 @@ pair<unsigned long long, vector<int>> default_dijksrta(const vector<container_of
             continue;
 
         for(auto [u, w] : g[v]){
-            if(d + w < distance[u]){
+            if(deleted_vertex.count(u) == 0 && d + w < distance[u]){
                 distance[u] = d + w;
                 q.push({distance[u], u});
                 parent[u] = v;
@@ -64,108 +68,113 @@ namespace std {
 }
 
 
-// структура для поддержки путей в алгоритме Йена
-struct Path{
-    unsigned long long distance;
-    vector<int> order;
-    unordered_set<pair<pair<int, int>, unsigned long long>> deleted_edges;
-
-    Path() :  
-        distance(0),
-        order({}),
-        deleted_edges({})
-    {}
-
-    Path(const pair<unsigned long long, vector<int>> &p) :
-        distance(p.first),
-        order(p.second),
-        deleted_edges({})
-    {}
-
-    Path(const pair<unsigned long long, vector<int>> &p, const unordered_set<pair<pair<int, int>, unsigned long long>> &deleted_edges) :
-        distance(p.first),
-        order(p.second),
-        deleted_edges(deleted_edges)
-    {}
-
-    bool operator < (const Path &another){
-        if(distance != another.distance) return distance < another.distance;
-        return order < another.order;
-    }
-
-    bool operator == (const Path &another){
-        return distance == another.distance && order == another.order;
-    }
-};
-
 // возвращает отсортированный список из number_of_paths пар вида {distance, path}, где
 // distance -- длина очередного кратчайшего пути
 // path -- сам путь
 
 vector<pair<unsigned long long, vector<int>>> yens_algorithm(const vector<vector<pair<int, unsigned long long>>> &g_vector, size_t number_of_paths){
-    vector<Path> chosen(number_of_paths);
-    chosen[0] = Path(default_dijksrta(g_vector));
+    // структура для поддержки путей в алгоритме Йена
+    struct Path{
+        unsigned long long distance;
+        vector<int> order;
+        unordered_set<pair<pair<int, int>, unsigned long long>> deleted_edges;
+
+        Path() :  
+            distance(0),
+            order({}),
+            deleted_edges({})
+        {}
+
+        Path(const pair<unsigned long long, vector<int>> &p) :
+            distance(p.first),
+            order(p.second),
+            deleted_edges({})
+        {}
+
+        Path(const pair<unsigned long long, vector<int>> &p, const unordered_set<pair<pair<int, int>, unsigned long long>> &deleted_edges) :
+            distance(p.first),
+            order(p.second),
+            deleted_edges(deleted_edges)
+        {}
+
+        pair<unsigned long long, vector<int>> to_vector(){
+            return {distance, order};
+        }
+
+        bool operator < (const Path &another){
+            if(distance != another.distance) return distance < another.distance;
+            return order < another.order;
+        }
+
+        bool operator == (const Path &another){
+            return distance == another.distance && order == another.order;
+        }
+    };
 
     size_t n = g_vector.size();
+
+    vector<Path> candidates;
+    vector<Path> chosen = {Path(default_dijksrta(g_vector, 0, *(new unordered_set<int>())))};
+
     unordered_map<pair<int, int>, unsigned long long> g_matrix;
 
-    for(size_t v = 0; v < n; v++)
+    for(int v = 0; v < n; v++)
         for(auto [u, w] : g_vector[v])
             g_matrix[{v, u}] = w;
 
-    vector<Path> candidates = {};
-
     for(size_t k = 1; k < number_of_paths; k++){
-        // поскольку в алгоритме Йена необходимо удалять/добавлять ребра в граф, воспользуемся
-        // хеш-таблицей, время выполнения указанных операций в которой равно О(1) в среднем
-        auto get_graph_without_deleted_edges{
-            [&g_vector, n](unordered_set<pair<pair<int, int>, unsigned long long>> &deleted_edges){
-                vector<unordered_set<pair<int, unsigned long long>>> res(n);
+        unordered_set<int> deleted_vertex = {};
+        const auto& p = chosen.back();
+        auto deleted_edges = p.deleted_edges;
+        vector<unordered_set<pair<unsigned long long, int>>> g_set(n);
 
-                for(size_t v = 0; v < n; v++)
-                    for(auto [u, w] : g_vector[v])
-                        if(deleted_edges.count({{v, u}, w}) == 0)
-                            res[v].insert({u, w});
+        for(int v = 0; v < n; v++)
+            for(auto [u, w] : g_vector[v])
+                if(!deleted_edges.count({{v, u}, w}))
+                    g_set[v].insert({u, w});
 
-                return res;
-            }
-        };
+        unsigned long long added_distance = 0;
+        vector<int> prefix = {};
 
-        auto &pth = chosen[k - 1];
-        auto del = pth.deleted_edges;
-
-        auto g_set = get_graph_without_deleted_edges(del);
-
-        for(size_t i = 0; i + 1 < pth.order.size(); i++){
-            auto v = pth.order[i];
-            auto u = pth.order[i + 1];
+        for(int i = 0; i + 1 < p.order.size(); i++){
+            auto v = p.order[i];
+            auto u = p.order[i + 1];
             auto w = g_matrix[{v, u}];
-
-            del.insert({{v, u}, w});
             g_set[v].erase({u, w});
+            deleted_edges.insert({{v, u}, w});
 
-            candidates.push_back(Path(default_dijksrta(g_set), del));
+            auto pp = default_dijksrta(g_set, v, deleted_vertex);
 
-            del.erase({{v, u}, w});
-            g_set[v].insert({u, w});
+            if(pp.first != INF64){
+                vector<int> real_order = prefix;
+                real_order.insert(real_order.end(), pp.second.begin(), pp.second.end());
+                candidates.push_back(Path({added_distance + pp.first, real_order}, deleted_edges));
+            }
+
+            deleted_edges.erase({{v, u}, w});
+            added_distance += w;
+            deleted_vertex.insert(v);
+            prefix.push_back(v);
         }
-
-        if(candidates.size() == 0)
-            break;
 
         sort(candidates.begin(), candidates.end());
         candidates.resize(unique(candidates.begin(), candidates.end()) - candidates.begin());
-        chosen[k] = candidates[0];
-        candidates.erase(candidates.begin());
-    }
 
-    while(chosen.back().order.size() == 0)
-        chosen.pop_back();
+        while(candidates.size() > number_of_paths)
+            candidates.pop_back();
+
+        if(candidates.size()){
+            chosen.push_back(candidates[0]);
+            candidates.erase(candidates.begin());
+        }else{
+            break;
+        }
+    }
 
     vector<pair<unsigned long long, vector<int>>> result(chosen.size());
 
-    for(size_t i = 0; i < chosen.size(); i++)
-        result[i] = {chosen[i].distance, chosen[i].order};
+    for(int i = 0; i < chosen.size(); i++)
+        result[i] = chosen[i].to_vector();
 
     return result;
 }
